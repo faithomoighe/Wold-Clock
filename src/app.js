e08464fede26ac0c81737fd12e2f4a7fe15a31f1
Akhigbe1010 <Akhigbefaithbright@gmail.com>
2023-03-04 15:36:22 +0100

    Single location was added

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
-alert("hello")
+//America/Los_Angeles
+function updateTime(){
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss:SS[<small>]A[</small]"
+  );
+
+  //lagos
+
+  let lagosElement = document.querySelector("#lagos");
+  let lagosDateElement = lagosElement.querySelector(".date");
+  let lagosTimeElement = lagosElement.querySelector(".time");
+  lagosTime = moment().tz("Africa/Lagos");
+
+  lagosDateElement.innerHTML = lagosTime.format("MMMM Do YYYY");
+  lagosTimeElement.innerHTML = lagosTime.format("h:mm:ss:SS[<small>]A[</small]");
+}
+updateTime();
+setInterval(updateTime, 100);
+
