5479c57404307aac06db842f2045315a28138358
Akhigbe1010 <Akhigbefaithbright@gmail.com>
2023-03-06 10:03:00 +0100

    Added the functionality for the selected city

--- src/app.js
+++ src/app.js
@@ -92,13 +92,14 @@ function updateTime() {
 }
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  let cityName= cityTimeZone.replace("-", "").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
        <div class="city">
         <div>
-        <h2>${cityTimeZone}</h2>
-        <div class="date">${cityTime.format("MMMM dddd Do YYYY")}</div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
         </div>
         <div class="time">${cityTime.format("h:mm:ss:SS")}
         <small>${cityTime.format("A")}</small></div>
